71b89b595a0db93864f586763132070401139580
DieEin <savageshadow@abv.bg>
2016-05-16 15:08:52 +0300

    minor changes

--- Project/app/src/main/java/com/example/tpproject/project/Fight.java
+++ Project/app/src/main/java/com/example/tpproject/project/Fight.java
@@ -29,7 +29,7 @@ public class Fight extends AppCompatActivity {
     }
 
     public static int Dragon_HP = 1000;
-    public static int Player_HP = 1000;
+    //public static int Player_HP = 1000;
 
 
 
@@ -42,45 +42,58 @@ public class Fight extends AppCompatActivity {
         ImageView Hitting = (ImageView)findViewById(R.id.hits);
 
         ProgressBar PlayerHPBar = (ProgressBar) findViewById(R.id.playerhp);
-        PlayerHPBar.setProgress(Player_HP);
+        PlayerHPBar.setProgress(PlayerScreen.health);
         ProgressBar DragonHPBar = (ProgressBar) findViewById(R.id.dragonhp);
         DragonHPBar.setProgress(Dragon_HP);
 
         while(true){
+            int k = 0;
+            for(int i = 0; i < 10000; i++) { k = i; }
             ManSwing.setImageResource(R.drawable.warrior);
             Hitting.setImageResource(R.drawable.start);
 
+            for(int i = 0; i < 1500000; i++) { k = i; }
             ManSwing.setImageResource(R.drawable.warrior2);
             Hitting.setImageResource(R.drawable.manhit);
             Dragon_HP = Dragon_HP - randomizeInteger(5, 10);
             DragonHPBar.setProgress(Dragon_HP);
 
+            for(int i = 0; i < 1500000; i++) { k = i; }
             ManSwing.setImageResource(R.drawable.warrior);
             Hitting.setImageResource(R.drawable.draghit);
-            Player_HP = Player_HP - randomizeInteger(5, 10);
-            PlayerHPBar.setProgress(Player_HP);
+            PlayerScreen.health = PlayerScreen.health - randomizeInteger(5, 10);
+            PlayerHPBar.setProgress(PlayerScreen.health);
 
-            if(Dragon_HP <= 0 && Player_HP > 0){
+            if(Dragon_HP <= 0 && PlayerScreen.health > 0){
                 String PlayerWon = "You WON!";
                 Toast.makeText(Fight.this, PlayerWon, Toast.LENGTH_SHORT).show();
                 int gainedMoney = randomizeInteger(20, 150);
                 int gainedXP = randomizeInteger(5, 35);
                 PlayerScreen.money += gainedMoney;
                 PlayerScreen.xp += gainedXP;
+
+                Dragon_HP = 100;
+
                 break;
-            }else if(Player_HP <= 0 && Dragon_HP > 0){
+            }else if(PlayerScreen.health <= 0 && Dragon_HP > 0){
                 String DragonWon = "You LOST! You don't gain any money!";
                 Toast.makeText(Fight.this, DragonWon, Toast.LENGTH_SHORT).show();
                 int gainedXP = randomizeInteger(5, 35);
                 PlayerScreen.xp += gainedXP / 5;
+
+                Dragon_HP = 100;
+
                 break;
-            }else if (Dragon_HP <= 0 && Player_HP <= 0) {
+            }else if (Dragon_HP <= 0 && PlayerScreen.health <= 0) {
                 String FightDraw = "Draw!";
                 Toast.makeText(Fight.this, FightDraw, Toast.LENGTH_SHORT).show();
                 int gainedMoney = randomizeInteger(20, 150);
                 int gainedXP = randomizeInteger(5, 35);
                 PlayerScreen.money += gainedMoney/5;
                 PlayerScreen.xp += gainedXP / 2;
+
+                Dragon_HP = 100;
+
                 break;
             }
         }
